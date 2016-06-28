88edfe39c6acc4f99d351621f2f359274239c91b
SingingChen <docterwater@gmail.com>
2016-06-28 13:41:19 +0800

    新增 屬性 建構子

--- start.php
+++ start.php
@@ -16,7 +16,26 @@ and open the template in the editor.
         
         class Cat 
         {
-           
+            public $name;//屬性
+            protected $position;
+            
+            public function __construct($name)//方法 建構子
+                    {
+                $this-> name = $name;
+                $this->position=['x'=>0, 'y'=>0];
+            }
+            public function  moveTo($x,$y)
+            {
+                $this->position['x']=$x;
+                $this->position['y']=$y;
+                
+                return$this->position;             
+            }    
+            public function resetPosition()
+            {
+                $this->moveTo(0, 0);
+                return $this->position;
+            }
         }
         
        
