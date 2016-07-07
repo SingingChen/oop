6f0a3bb82a1619c50e5292b048b92c1d6d0d1b0f
SingingChen <docterwater@gmail.com>
2016-07-07 09:07:35 +0800

    改名字讓可讀性更好

--- polymorphism.php
+++ polymorphism.php
@@ -1,17 +1,23 @@
 <?php
 interface dataTransferImterface//定義網站傳送資料
 {
-    public function dataID();//資料ID
-    public function data();//資料內容
+    public function getdataID();//資料ID
+    public function getdata();//資料內容
 }
 class dataStorage//網站獲得 存取資料用
 {
-
+    protected $dataArray =[];//宣告存放資料用的陣列
     public function addData(dataTransferImterface $dataTransfer)//取得資料方法
     {
-        protected $dataArray =[];
-       $arrayKey= $dataTransfer->dataID();
-        $arrayValue=$dataTransfer->data();
+
+       $arrayKey= $dataTransfer->getdataID();
+        $arrayValue=$dataTransfer->getdata();
         $this->dataArray[$arrayKey]=$arrayValue;
+    }
+
+
+    public function getDataArray()
+    {
+        return $this->dataArray;
     }
 }
