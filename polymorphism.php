9e532ae0a77af5ac1c1dd172cd016c8c1ce08086
SingingChen <docterwater@gmail.com>
2016-07-07 08:58:58 +0800

    最初polymorphism

---
+++ polymorphism.php
@@ -0,0 +1,15 @@
+<?php
+interface dataTransferImterface//定義網站傳送資料
+{
+    public function dataID();//資料ID
+    public function data();//資料內容
+}
+class dataStorage//網站獲得 存取資料用
+{
+
+    public function addData(dataTransferImterface $dataTransfer)//取得資料方法
+    {
+        $dataTransfer->dataID();
+        $dataTransfer->data();
+    }
+}
