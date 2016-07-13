6d3fed9107006f4daee33f231a47218d8ae0ca9c
SingingChen <docterwater@gmail.com>
2016-07-13 09:04:50 +0800

    多載

---
+++ overloading.php
@@ -0,0 +1,22 @@
+<?php
+class A{
+  function __get($undefineVar)//屬性沒有定義時 會進入到這個 magic method,可以屬性多載
+  {
+    if($undefineVar === "var1")//設置可以依據屬性名稱進一步的處理
+    return "觸發 magic method __get()<br>";
+   }
+    function  __call($methodName, $methodParameter)//方法沒有定義時 會進入這麼magic method 可以作方法的多載
+    {
+      echo "未定義的方法參數".$methodName."(".print_r($methodParameter,true).")<br>";
+    }
+}
+
+$a=new A();
+//動態宣告
+echo $a->var1;//會觸發可用在屬性多載的  magic method 因為沒有在定義類別中
+
+$a->undefinedMethod("參數1","參數2");//會觸發可用在屬性多載的  magic method 因為沒有在定義類別中
+
+echo"~end~";
+
+?>
