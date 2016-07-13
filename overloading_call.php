a163817a6af96cbb6088fdda7704c57873ec059c
SingingChen <docterwater@gmail.com>
2016-07-13 09:31:27 +0800

    __call用在多個猜數  (array)

---
+++ overloading_call.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+
+        class a {
+            function __call($name, $args) {
+                echo $name . " : " . print_r($args, true) . "\n";
+            }
+        }
+        $a = new a;
+        $a->func1('abc', 'def');
+        echo "<br>";
+
+        class b {
+            function __call($name, $args) {
+                switch($name) {
+                    case 'add':
+                        if(count($args)===2) {
+                            if(is_numeric($args[0]) && is_numeric($args[1]))
+                                return $args[0]+$args[1];
+                            if(is_string($args[0]) && is_string($args[1]))
+                                return $args[0].$args[1];
+                        }else return "飛飛飛<br>";
+                        
+                    default:
+                        throw new Exception("[warning] b::$name method not found.\n");
+                }
+            }
+        }
+
+        $b = new b;
+        echo $b->add(2,3)."\n<br>";
+        echo $b->add('hello', ' world.')."\n";
+        echo "<br>";
+        echo $b->add(2,3,4)."\n";
+        
+        try {
+            echo $b->add(2, ' world.')."\n";  
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+        ?>
+    </body>
+</html>
